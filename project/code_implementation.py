db1f5c0742d133e96b94246549a532d0e4a50a45
joaoBrRj91 <joao1991algoritmo@gmail.com>
2025-06-23 17:27:50 -0300

    Implement OpenAI API interaction and response handling in code_implementation.py

--- project/code_implementation.py
+++ project/code_implementation.py
@@ -10,6 +10,20 @@ from openai.types.responses import Response
 
 messages = [{"role": "user", "content": "Write a one-sentence bedtime story about a unicorn."}]
 
+
+# Function to call the OpenAI API and get a response
+def model_api_response_call(messages_input:list) -> Response:
+    """
+    Function to call the OpenAI API and get a response.
+    """
+    client = OpenAI()
+    return client.responses.create(
+        model=os.getenv("MODEL"),
+        temperature=float(os.getenv("TEMPERATURE")),
+        max_output_tokens=int(os.getenv("MAX_TOKENS")),
+        input=messages_input
+    )
+
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 def response_model_output_generated(response_model:Response) -> ResponseOutputMessage:
     """
@@ -23,18 +37,20 @@ def response_model_output_generated(response_model:Response) -> ResponseOutputMe
         message=response_model.output[0].message
     )
 
+def response_model_append_message(response_model:Response) -> Response:
+    """
+    Function to append a message to the response model.
+    """
+    response_model.output.append(response_model_output_generated(response_model))
+    return response_model
+
 
 # Carregar as variáveis de ambiente do arquivo .env
 _ = load_dotenv(find_dotenv())
 
-client = OpenAI()
+# Logic for get message from console input user
 
-response = client.responses.create(
-    model=os.getenv("MODEL"),
-    temperature= os.getenv("TEMPERATURE"),
-    max_output_tokens= os.getenv("MAX_TOKENS"),
-    input=messages
-)
+response_model_append_message(model_api_response_call(messages))
 
-# Print the response object
-print(response_model_output_generated(response))
+# Print the current messages after processing the response
+print(messages)
